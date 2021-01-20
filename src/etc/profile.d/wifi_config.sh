94d352ce131eb1c7bffcb488650b858a5971d3d4
David C. Drake <theDrake@users.noreply.github.com>
2021-01-20 03:19:30 -0700

    Update wifi_config.sh

--- src/etc/profile.d/wifi_config.sh
+++ src/etc/profile.d/wifi_config.sh
@@ -1,3 +1,8 @@
+# To autoconnect the installer to Wi-Fi, uncomment and define these variables:
+
+#SSID="wifi_network_ssid"
+#PASSWORD="wifi_network_password"
+
 if [ ${SSID} ] && [ ${PASSWORD} ]; then
-    nmcli dev wifi connect "${SSID}" password "${PASSWORD}"
+  nmcli dev wifi connect "${SSID}" password "${PASSWORD}"
 fi
