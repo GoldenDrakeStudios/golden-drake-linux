4f5417854253daeec907cea6d8577b95c084a635
David C. Drake <theDrake@users.noreply.github.com>
2021-07-07 22:03:36 -0600

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 
 iso_name="gdl"
 iso_label="GDL-001"
@@ -6,10 +7,12 @@ iso_publisher="Golden Drake Studios <https://goldendrakestudios.com>"
 iso_application="Golden Drake Linux - Arch Linux Installer"
 iso_version="0.0.1"
 install_dir="gdl"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+  'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size'
+  '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
