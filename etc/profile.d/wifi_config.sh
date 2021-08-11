dfc091c505fce8143e8d258798f2116418c12f5a
David C. Drake <theDrake@users.noreply.github.com>
2021-08-10 23:05:08 -0600

    Add gdl.conf, adjust Wi-Fi management, etc.

--- etc/profile.d/wifi_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# To auto-connect the GDL installer to Wi-Fi, provide an SSID and password:
-
-WIFI_SSID=""
-WIFI_PASSWORD=""
-
-if [[ -n "${WIFI_SSID}" && -n "${WIFI_PASSWORD}" ]]; then
-  nmcli dev wifi connect "${WIFI_SSID}" password "${WIFI_PASSWORD}"
-fi
