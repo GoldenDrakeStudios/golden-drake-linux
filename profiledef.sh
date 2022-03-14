617d288a028b3ee9ca11168080acb6e329a89e1b
David C. Drake <theDrake@users.noreply.github.com>
2022-03-13 23:08:31 -0600

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -5,22 +5,28 @@ iso_name="gdl"
 iso_label="GDL_$(date +%Y%m%d)"
 iso_publisher="Golden Drake Studios <https://goldendrakestudios.com>"
 iso_application="Golden Drake Linux - Arch Linux Installer"
-iso_version="1.0.0"
+iso_version="$(grep ^GDL_VER airootfs/etc/gdl.conf | tr -d "GDL_VERSION='")"
 install_dir="gdl"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-  'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=(
+  'bios.syslinux.mbr'
+  'bios.syslinux.eltorito'
+  'uefi-x64.systemd-boot.esp'
+  'uefi-x64.systemd-boot.eltorito'
+)
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size'
-  '1M')
+airootfs_image_tool_options=(
+  '-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M'
+)
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/bin/gdl"]="0:0:755"
+  ["/etc/shadow"]="0:0:0400"
+  ["/root"]="0:0:0750"
+  ["/root/.automated_script.sh"]="0:0:0755"
+  ["/usr/local/bin/choose-mirror"]="0:0:0755"
+  ["/usr/local/bin/Installation_guide"]="0:0:0755"
+  ["/usr/local/bin/livecd-sound"]="0:0:0755"
+  ["/usr/share/gdl/extra/xfce/toggle-touchpad"]="0:0:0755"
+  ["/usr/bin/gdl"]="0:0:0755"
 )
