0cbb5bb22a17e02282f2b9fe09e8a0b705edf514
David C. Drake <theDrake@users.noreply.github.com>
2025-10-30 07:54:09 -0600

    Update `profiledef.sh`

--- profiledef.sh
+++ profiledef.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Profile for ISO creation. Modified from 'configs/releng/profiledef.sh' here:
+# https://gitlab.archlinux.org/archlinux/archiso/
+#
 # shellcheck disable=SC2034
 
 iso_name="gdl"
@@ -10,25 +14,25 @@ iso_version="$(grep 'readonly GDL' airootfs/etc/gdl.conf \
 install_dir="gdl"
 buildmodes=('iso')
 bootmodes=(
-  'bios.syslinux.mbr'
-  'bios.syslinux.eltorito'
-  'uefi-x64.systemd-boot.esp'
-  'uefi-x64.systemd-boot.eltorito'
+  'bios.syslinux'
+  'uefi.systemd-boot'
 )
-arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=(
   '-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M'
 )
+bootstrap_tarball_compression=(
+  'zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19'
+)
 file_permissions=(
-  ["/etc/shadow"]="0:0:0400"
-  ["/root"]="0:0:0750"
-  ["/root/.gnupg"]="0:0:0700"
-  ["/root/.automated_script.sh"]="0:0:0755"
-  ["/usr/local/bin/choose-mirror"]="0:0:0755"
-  ["/usr/local/bin/Installation_guide"]="0:0:0755"
-  ["/usr/local/bin/livecd-sound"]="0:0:0755"
-  ["/usr/share/gdl/extra/xfce/toggle-touchpad"]="0:0:0755"
-  ["/usr/bin/gdl"]="0:0:0755"
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/share/gdl/extra/xfce/toggle-touchpad"]="0:0:755"
+  ["/usr/bin/gdl"]="0:0:755"
 )
